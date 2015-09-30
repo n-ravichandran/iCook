300bb9445e1dbb0a0710f844be65f3d5f197bb8e
Niranjan Ravichandran <niranjanravichandran@live.com>
2015-09-29 20:17:19 -0400

    Initial Commit

---
+++ iCook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCook
+//
+//  Created by Niranjan Ravichandran on 29/09/15.
+//  Copyright © 2015 Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
